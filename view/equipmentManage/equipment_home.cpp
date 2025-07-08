18273535893c730f1b607506577215559c2ecd40
lyx12396 <2823809412@qq.com>
2025-07-08 15:33:49 +0800

    非ui界面搭建

--- view/equipmentManage/equipment_home.cpp
+++ view/equipmentManage/equipment_home.cpp
@@ -9,6 +9,7 @@
 #include "kdeletemanage.h"
 #include "applysee.h"
 #include "exam.h"
+#include <QStandardItemModel>
 
 namespace view::equipment {
     equipment_home::equipment_home(QWidget *parent) : QWidget(parent), ui(new view::equipment::Ui::equipment_home) {
@@ -74,27 +75,119 @@ namespace view::equipment {
         // TODO: 实现检查和审批审查功能
     }
 
-    void equipment_home::on_kseeborrow_clicked() //看查借用日志
+    void equipment_home::on_kseeborrow_clicked()
     {
-        // TODO: 实现查看借用日志功能
+        // 创建一个对话框
+        QDialog dialog(this);
+        dialog.setWindowTitle("借用日志");
+
+        // 创建一个 QTableView
+        QTableView tableView;
+
+        // 创建一个模型（示例中使用 QStandardItemModel）
+        QStandardItemModel model(0, 3); // 3 列：ID、设备名称、借用日期
+        model.setHorizontalHeaderLabels(QStringList() << "ID" << "设备名称" << "借用日期");
+
+        // 添加示例数据
+        QList<QStandardItem *> items;
+        for (int i = 0; i < 5; ++i) { // 添加5行示例数据
+            items.append(new QStandardItem(QString::number(i + 1)));
+            items.append(new QStandardItem("设备 " + QString::number(i + 1)));
+            items.append(new QStandardItem("2024-11-01"));
+            model.appendRow(items);
+            items.clear(); // 清空 items 以准备下一行
+        }
+
+        // 设置模型到 QTableView
+        tableView.setModel(&model);
+
+        // 创建布局并添加 QTableView
+        QVBoxLayout layout(&dialog);
+        layout.addWidget(&tableView);
+
+        // 显示对话框
+        dialog.setLayout(&layout);
+        dialog.exec();
     }
 
-    void equipment_home::on_seefix_clicked() //看查维修日志
+    void equipment_home::on_seefix_clicked()
     {
-        // TODO: 实现查看维修日志功能
+        QDialog dialog(this);
+        dialog.setWindowTitle("维修日志");
+
+        QTableView tableView;
+        QStandardItemModel model(0, 3); // 3 列：ID、设备名称、维修日期
+        model.setHorizontalHeaderLabels(QStringList() << "ID" << "设备名称" << "维修日期");
+
+        // 添加示例数据
+        QList<QStandardItem *> items;
+        for (int i = 0; i < 3; ++i) {
+            items.append(new QStandardItem(QString::number(i + 1)));
+            items.append(new QStandardItem("设备 " + QString::number(i + 1)));
+            items.append(new QStandardItem("2024-10-15"));
+            model.appendRow(items);
+            items.clear();
+        }
+
+        tableView.setModel(&model);
+
+        QVBoxLayout layout(&dialog);
+        layout.addWidget(&tableView);
+        dialog.setLayout(&layout);
+        dialog.exec();
     }
 
-    void equipment_home::on_outdata_clicked() //超期预警
+    void equipment_home::on_outdata_clicked()
     {
-        // TODO: 实现超期预警功能
+        QDialog dialog(this);
+        dialog.setWindowTitle("超期预警");
+
+        QTableView tableView;
+        QStandardItemModel model(0, 3); // 3 列：ID、设备名称、超期天数
+        model.setHorizontalHeaderLabels(QStringList() << "ID" << "设备名称" << "超期天数");
+
+        QList<QStandardItem *> items;
+        for (int i = 0; i < 2; ++i) {
+            items.append(new QStandardItem(QString::number(i + 1)));
+            items.append(new QStandardItem("设备 " + QString::number(i + 1)));
+            items.append(new QStandardItem(QString::number(i + 5))); // 超期天数
+            model.appendRow(items);
+            items.clear();
+        }
+
+        tableView.setModel(&model);
+
+        QVBoxLayout layout(&dialog);
+        layout.addWidget(&tableView);
+        dialog.setLayout(&layout);
+        dialog.exec();
     }
 
 
-    void equipment_home::on_blacklist_clicked() //访问黑名单
+    void equipment_home::on_blacklist_clicked()
     {
-        // TODO: 实现访问黑名单功能
-    }
+        QDialog dialog(this);
+        dialog.setWindowTitle("黑名单");
 
+        QTableView tableView;
+        QStandardItemModel model(0, 2); // 2 列：用户ID、用户名
+        model.setHorizontalHeaderLabels(QStringList() << "用户ID" << "用户名");
+
+        QList<QStandardItem *> items;
+        for (int i = 0; i < 4; ++i) {
+            items.append(new QStandardItem(QString::number(i + 1001)));
+            items.append(new QStandardItem("用户 " + QString::number(i + 1)));
+            model.appendRow(items);
+            items.clear();
+        }
+
+        tableView.setModel(&model);
+
+        QVBoxLayout layout(&dialog);
+        layout.addWidget(&tableView);
+        dialog.setLayout(&layout);
+        dialog.exec();
+    }
 
     void equipment_home::on_searchbtn_clicked() //索引
     {
