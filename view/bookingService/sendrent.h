c8769c5d31a5042001a2323fda96a3e7ae80b017
KaaNoo <zh1186853628@outlook.com>
2025-07-10 11:56:25 +0800

    合并代码

--- view/bookingService/sendrent.h
+++ view/bookingService/sendrent.h
@@ -6,7 +6,7 @@
 #define SENDRENT_H
 
 #include <QDialog>
-
+#include "module/data/data_mail.h"
 namespace view::Order {
 QT_BEGIN_NAMESPACE
 namespace Ui { class SendRent; }
@@ -19,6 +19,8 @@ public:
     explicit SendRent(const QString& name,const QString& id,const QString& devName,QWidget *parent = nullptr);
     explicit SendRent(const QString& name,const QString& id,QWidget* parent = nullptr);
     ~SendRent() override;
+public slots:
+    void on_btnSend_clicked();
 private:
     Ui::SendRent *ui;
 };
