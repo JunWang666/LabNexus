784411b9076a2458b1b1b35de6eac2b46270debd
KaaNoo <zh1186853628@outlook.com>
2025-07-10 17:11:14 +0800

    apply新增催一催功能

--- view/bookingService/apply.cpp
+++ view/bookingService/apply.cpp
@@ -7,7 +7,7 @@
 #include "apply.h"
 #include "ui_Apply.h"
 #include "module/model/EquipmentDataModel.h"
-
+#include "module/data/data_mail.h"
 namespace view::Order {
 Apply::Apply(QWidget *parent) :
     QWidget(parent), ui(new Ui::Apply) {
@@ -62,4 +62,49 @@ void Apply::on_btnClose_clicked()
     this->close();
 }
 
+void Apply::on_btnRush_clicked() {
+    if (teacherId =="") {
+        setApprover = new SetApprover(teacherId,this);
+        setApprover->show();
+    }
+    else {
+        QItemSelectionModel *selectionModel = ui->applyTableView->selectionModel();
+        if (selectionModel->hasSelection()) {//如果有单元格被选择
+            QModelIndexList  indexes = selectionModel->selectedIndexes();//获取index
+            QModelIndex proxyIndex = indexes.first();
+            QModelIndex index = fliterModel->mapToSource(proxyIndex);
+            QString senderId = fliterModel->data(fliterModel->index(index.row(),dataModel::BookingDataModel::Col_Id)).toString();
+            QString receiverId = teacherId;
+            QDateTime startTime = fliterModel->data(fliterModel->index(index.row(),dataModel::BookingDataModel::Col_RequestStartDate)).toDateTime();
+            QDateTime endTime = fliterModel->data(fliterModel->index(index.row(),dataModel::BookingDataModel::Col_RequestEndDate)).toDateTime();
+            // QString type  =fliterModel->data(fliterModel->index(index.row(),dataModel::BookingDataModel::Col_));
+            QString subject = "申请";
+            QString senderName;
+            auto resultSenderName = data::UserControl::Login::foundUserIdByIdNumber(senderId);
+            if (resultSenderName.has_value()) {
+                int id = resultSenderName.value();
+                auto resultSenderName1 = data::UserControl::UserInfo::getUserNameById(id);
+                if (resultSenderName1.has_value()) {
+                    senderName = resultSenderName1.value();
+                }
+            }
+            QString receiverName;
+            auto resultReceiverName = data::UserControl::Login::foundUserIdByIdNumber(receiverId);
+            if (resultReceiverName.has_value()) {
+                int id = resultReceiverName.value();
+                auto resultReceiverName1 = data::UserControl::UserInfo::getUserNameById(id);
+                if (resultReceiverName1.has_value()) {
+                    receiverName = resultReceiverName1.value();
+                }
+            }
+            QString content = tr("%1向%2申请%3设备").arg(senderName).arg(receiverName).arg("");
+            data::mail::send_mail(senderId.toInt(),receiverId.toInt(),subject,content,"");
+        }
+        else {
+            QMessageBox::warning(this,"警告","请选择一个申请",QMessageBox::Ok);
+        }
+    }
+
+
+}
 } // view::Order
