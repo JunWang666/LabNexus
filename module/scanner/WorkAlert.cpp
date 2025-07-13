74dcea21c2f8606e9a8b3d5bb59da782b4b6ccdb
Gouzuang <87839812+Gouzuang@users.noreply.github.com>
2025-07-13 16:44:03 +0800

    修复工作提醒邮件发送逻辑，调整发件人与收件人顺序

--- module/scanner/WorkAlert.cpp
+++ module/scanner/WorkAlert.cpp
@@ -11,8 +11,8 @@ namespace bot::WorkAlert {
         if (!data::UserControl::permission::isUserInGroup(data::UserControl::currentUserId, "Admin")) return;
         auto count = data::UserControl::check::getUncheckedUserCount();
         if (count == 0) return;
-        data::mail::send_mail(data::UserControl::currentUserId,
-                              data::mail::systemReservedAccounts["工作提醒"],
+        data::mail::send_mail(data::mail::systemReservedAccounts["工作提醒"],
+                              data::UserControl::currentUserId,
                               "还有" + QString::number(count) + "个新用户待审核",
                               "请前往注册中心查看新用户信息并进行审核。",
                               "{}");
